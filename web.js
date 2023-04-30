61a37df1c37c8879790fdd6bcb8d3332cf603ae4
aretty <aretty_company@naver.com>
2023-05-01 08:07:30 +0900

    main modify

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
 const express = require('express')
+const http = require("http")
 const app = express()
 const PORT = 8001
+const server = http.createServer(app)
+
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
   res.render('index')
 })
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`server started on PORT ${PORT}`)
 })
 // web.js
