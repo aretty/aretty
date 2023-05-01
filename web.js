00be82e0f42a987bb62d63ae68f7d5d588e8d0df
aretty <aretty_company@naver.com>
2023-05-01 09:25:40 +0900

    resources use test2

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 const express = require('express')
 // const http = require("http")
 const app = express()
+const path = require("path")
 const PORT = 8001
 // const server = http.createServer(app)
 // const socketIO = require("socket.io")
